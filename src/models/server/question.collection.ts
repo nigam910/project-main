65c1722d9cac651f834271ba187c64ac017d5fd6
nigam910 <nigamdhillo3021@gmail.com>
2024-08-16 16:51:15 +0530

    fix bug

--- src/models/server/question.collection.ts
+++ src/models/server/question.collection.ts
@@ -2,21 +2,25 @@ import {Permission} from "node-appwrite"
 
 import {db, questionCollection} from "../name"
 import { databases } from "./config"
-import { permission } from "process"
+
 
 
 
 export default async function createQuestionCollection(){
     //create collection 
     await databases.createCollection(db, questionCollection, questionCollection, [
-        permission.read("any"),
-        permission.read("users"),
-        permission.create("users"),
-        permission.update("users"),
-        permission.delete("users"),
+        Permission.read("any"),
+        Permission.read("users"),
+        Permission.create("users"),
+        Permission.update("users"),
+        Permission.delete("users"),
 
 
 
     ])
     console.log("Question collection is created")
+
+    //creating attributes and Indexes
+
+    databases.createStringAttribute(db, questionCollection, "title", 100, true)
 }
